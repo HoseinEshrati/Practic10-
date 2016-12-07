ea6ef221af7352e948d6165ec2d5a171b0031107
HOSEIN\Hosein <eshrati@outlook.com>
2016-12-07 14:27:17 +0330

    final

--- Practice10/app/src/main/java/com/example/hosein/practice10/MainActivity.java
+++ Practice10/app/src/main/java/com/example/hosein/practice10/MainActivity.java
@@ -3,21 +3,30 @@ package com.example.hosein.practice10;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import java.util.Random;
 
+import static com.example.hosein.practice10.R.color.colorPrimary;
+
 public class MainActivity extends AppCompatActivity {
     int i;
     int counter = 1;
-    Button button[], btplus;
-    LinearLayout layout,layout1,layout2,layout3;
+    Button button[];
+    Button btplus;
+
+    LinearLayout layout, layout1, layout2, layout3,layout4,layout5;
 
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
         btplus = (Button) findViewById(R.id.plus);
         button = new Button[100];
@@ -25,12 +34,18 @@ public class MainActivity extends AppCompatActivity {
         layout1 = (LinearLayout) findViewById(R.id.lin2);
         layout2 = (LinearLayout) findViewById(R.id.lin3);
         layout3 = (LinearLayout) findViewById(R.id.lin4);
+        layout4 = (LinearLayout) findViewById(R.id.lin5);
+        layout5 = (LinearLayout) findViewById(R.id.lin6);
+      final Animation  anim2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim1);
+
         for (i = 1; i <= 4; i++) {
 
             button[i] = new Button(this);
             button[i].setTag(i);
-            button[i].setText("click");
+            button[i].setText("*****");
+
             layout.addView(button[i]);
+
 
         }
 
@@ -38,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
             button[i] = new Button(this);
             button[i].setTag(i);
-            button[i].setText("click");
+            button[i].setText("****");
             layout1.addView(button[i]);
 
         }
@@ -47,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
             button[i] = new Button(this);
             button[i].setTag(i);
-            button[i].setText("click");
+            button[i].setText("****");
             layout2.addView(button[i]);
 
         }
@@ -56,24 +71,34 @@ public class MainActivity extends AppCompatActivity {
 
             button[i] = new Button(this);
             button[i].setTag(i);
-            button[i].setText("click");
-            layout3.addView(button[i]);
+            button[i].setText("****");
+            layout4.addView(button[i]);
+
+        }
+        for (i = 16; i <= 19; i++) {
+
+            button[i] = new Button(this);
+            button[i].setTag(i);
+            button[i].setText("****");
+            layout5.addView(button[i]);
 
         }
 
-
         btplus.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-        button[counter].setText("goooo");
+              btplus.startAnimation(anim2);
+                button[counter].setText("okOK");
+                Random random = new Random();
+                int x = random.nextInt(6);
+                counter = counter + x;
 
-        Random random = new Random();
-        int x= random.nextInt(5);
-        counter = counter+x;
+            }
+        });
 
-    }
-});
+
+
     }
 
 
